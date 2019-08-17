715ae7f31d88992f925adb7b643e36cf0a9eb6e3
katchip <ericaxia3@gmail.com>
2019-08-16 19:08:22 -0700

    image upload

--- EDA.py
+++ EDA.py
@@ -25,7 +25,7 @@ from sklearn.model_selection import train_test_split
 # open and preprocess data
 pet_data = pd.read_csv('cleaned_df.csv')
 
-pet_data.head()
+print(pet_data.head())
 
 pet_data['Outcome Type'].value_counts(normalize=True) # approx 60% animals get adopted
 
@@ -41,17 +41,53 @@ euthanized["Intake Condition"].value_counts(normalize=True)
 
 euthanized[euthanized["Intake Condition"] == "HEALTHY"].shape
 
-
 # Age distribution
+plt.title('Age Distribution')
 sns.distplot(pet_data["Age"])
 
 # Age distribution by adopted v euthanized
-
-
-# Age distr by type
-
+# Draw Plot
+sns.distplot(pet_data.loc[pet_data['Outcome Type'] == 'EUTHANIZE', "Age"], color="dodgerblue", label="EUTHANIZE", hist_kws={'alpha':.7}, kde_kws={'linewidth':3})
+sns.distplot(pet_data.loc[pet_data['Outcome Type'] == 'ADOPTION', "Age"], color="orange", label="ADOPTION", hist_kws={'alpha':.7}, kde_kws={'linewidth':3})
+plt.title('Age Distribution by Outcome Type', fontsize=14)
+plt.legend()
+plt.show()
 
 # adopted v euthanized ; cats v dogs
+plt.title('Counts by Pet Type', fontsize=14)
+sns.countplot("Outcome Type", hue="Type", data=pet_data)
 
-# adopted v euthanized; colors
+
+# adopted v euthanized; different features coefficient values (post modeling)
+coef = pd.read_csv("coefficients.csv")
+print(coef.shape)
+coef.head()
+coef['features'] = coef['feature'].str.split('_').str[1]
+coef.drop(columns=['feature', 'Unnamed: 0'], inplace=True)
+
+coef.head()
+
+x = coef.loc[:, ['coef']]
+coef['coef_z'] = (x - x.mean())/x.std()
+coef = coef[(coef["coef_z"] < -0.4) | (coef["coef_z"] > 0.4)]
+coef['colors'] = ['red' if x < 0 else 'green' for x in coef['coef_z']]
+coef.sort_values('coef_z', inplace=True)
+coef.reset_index(inplace=True)
+
+
+coef.shape
+
+# Draw plot
+plt.figure(figsize=(12,12), dpi= 80)
+plt.hlines(y=coef.index, xmin=0, xmax=coef.coef_z)
+for x, y, tex in zip(coef.coef_z, coef.index, coef.coef_z):
+    t = plt.text(x, y, round(tex, 2), 
+    horizontalalignment='right' if x < 0 else 'left', verticalalignment='center', fontdict={'color':'red' if x < 0 else 'green', 'size':14})
+
+# Decorations    
+plt.yticks(coef.index, coef.features, fontsize=12)
+plt.title('Most Impactful Coefficients on Adoption Outcome', fontdict={'size':20})
+plt.grid(linestyle='--', alpha=0.5)
+plt.xlim(-2.5, 2.5)
+plt.show()
 
